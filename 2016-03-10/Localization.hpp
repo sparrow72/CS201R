9b1200bce0b5d83786f52351409c894e386689a2
Rachel J Morris <RachelJMorris@gmail.com>
2016-03-11 00:19:50 -0600

    Basic Localization code

--- 2016-03-10/Localization.hpp
+++ 2016-03-10/Localization.hpp
@@ -1,1 +1,19 @@
+#ifndef _LOCAL
+#define _LOCAL
 
+#include <map>
+#include <string>
+using namespace std;
+
+class Localization
+{
+public:
+	void LoadLanguage(const string& filename, const string& language);
+	string& operator[](const string& key);
+
+private:
+	map<string, string> m_text;
+	string m_language;
+};
+
+#endif
--- 2016-03-10/main.cpp
+++ 2016-03-10/main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+#include "Localization.hpp"
+
+int main()
+{
+	Localization english, esperanto;
+	english.LoadLanguage("C:\\CS201R\\2016-03-10\\english.txt", "English");
+	esperanto.LoadLanguage("C:\\CS201R\\2016-03-10\\esperanto.txt", "Esperanto");
+
+	cout << english["PROGRAM TITLE"] << endl;
+	cout << esperanto["PROGRAM TITLE"] << endl;
+
+	return 0;
+}
