da3eef21c307a208cd3a5da33b32ef28c1801a93
Brendan O'Connor <brendan.oconnor.913@gmail.com>
2016-03-17 18:26:09 -0500

    Three Blanks Question added

--- 2016-03-17/Quizzr/Question.hpp
+++ 2016-03-17/Quizzr/Question.hpp
@@ -1,6 +1,9 @@
 #ifndef _QUESTION
 #define _QUESTION
 
+#include <string>
+using namespace std;
+
 class IQuestion
 {
     public:
@@ -51,6 +54,19 @@ class Ordering : public IQuestion
 // Question where user must type in 3 answers, which will be compared to stored answer text
 class ThreeBlanks : public IQuestion
 {
+public:
+	ThreeBlanks() {};
+	void CreateQuestion();
+	void Display();
+	bool Answer();
+private:
+	string m_a1;
+	string m_a2;
+	string m_a3;
+	string m_r1;
+	string m_r2;
+	string m_r3;
+	string m_question;
 };
 
 #endif
