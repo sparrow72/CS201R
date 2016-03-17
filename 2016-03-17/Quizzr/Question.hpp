70a5417a5b720d34622fad51110317e8192f9864
sittonj <justin.sitton@outlook.com>
2016-03-17 18:26:54 -0500

    Fill in the blank

--- 2016-03-17/Quizzr/Question.hpp
+++ 2016-03-17/Quizzr/Question.hpp
@@ -36,6 +36,94 @@ class MultipleAnswer : public IQuestion
 // Question where user fills in one word as answer, compare to stored answer text
 class FillInTheBlank : public IQuestion
 {
+private:
+	int numberOfQuestions;
+	string* Questions;
+	string* Answers;
+	string* UserAnswers;
+
+	string m_question;
+	string m_answer;
+	string m_userAnswer;
+
+public:
+	FillInTheBlank()
+	{
+		numberOfQuestions = 1;
+
+		string* Questions = new string[numberOfQuestions];
+		string* Answers = new string[numberOfQuestions];
+		
+		m_question = " ";
+		m_answer = " ";
+	}
+
+	virtual ~ FillInTheBlank()
+	{
+		delete[] Questions;
+		delete[] Answers;
+		delete[] UserAnswers;
+
+		Questions = NULL;
+		Answers = NULL;
+		UserAnswers = NULL;
+	}
+
+	virtual void CreateQuestion()
+	{
+		bool checkInput = true;
+		bool addQuestion = true;
+		string userValidation;
+
+		while (addQuestion)
+		{
+			cout << "Please enter a Fill in the Blank Question" << endl;
+			getline(cin, m_question);
+			Questions[numberOfQuestions - 1] = m_question;
+
+			cout << "Answer: " << endl;
+			cin >> m_answer;
+			Answers[numberOfQuestions - 1] = m_answer;
+
+			cout << "Would you like to add another question? Yes or No";
+			cin >> userValidation;
+
+			while (checkInput)
+			{
+				if (userValidation == "No" || userValidation == "N" || userValidation == "n")
+				{
+					addQuestion = false;
+					checkInput = false;
+					string* UserAnswers = new string[numberOfQuestions];
+				}
+				else if ((userValidation == "Yes" || userValidation == "Y" || userValidation == "y"))
+				{
+					addQuestion = true;
+					checkInput = false;
+					numberOfQuestions++;
+				}
+				else
+				{
+					cout << "Please add a valid response (Yes or No)" << endl;
+				}
+			}
+		}
+		
+	}
+
+	virtual void Display()
+	{
+
+		for (int i = 0; i < numberOfQuestions; i++)
+		{
+			cout << Questions[i] << endl << endl;
+		}
+	}
+
+	virtual bool Answer()
+	{
+
+	}
 };
 
 // Question where user fills in a line of text as the answer, compare to stored answer text
