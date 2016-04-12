5fcdce9d52709bfa9dd4a163c1fd5ed209412c51
Rachel J Morris <racheljmorris@gmail.com>
2016-04-12 14:54:36 -0500

    Search

---
+++ 2016-04-12/Search/search.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int Find( const string& value, string arr[], int size )
+{
+    for ( int i = 0; i < size; i++ )
+    {
+        if ( value == arr[ i ] )
+            return i;
+    }
+    return -1;
+}
+
+int main()
+{
+    string animals[5] = { "cat", "dog", "ferret", "snake", "bird" };
+
+    int index1 = Find( "cat", animals, 5 );
+    int index2 = Find( "skunk", animals, 5 );
+
+    cout << "Cat at " << index1 << endl;
+    cout << "Skunk at " << index2 << endl;
+
+    return 0;
+}
