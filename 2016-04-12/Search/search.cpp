7acf449c8aaa65035b40de90bce2fef381707cd6
Rachel J Morris <racheljmorris@gmail.com>
2016-04-12 17:42:31 -0500

    Recursive find

--- 2016-04-12/Search/search.cpp
+++ 2016-04-12/Search/search.cpp
@@ -12,15 +12,59 @@ int Find( const string& value, string arr[], int size )
     return -1;
 }
 
+int Find( const string& value, string arr[], int size, int index )
+{
+    if ( value == arr[index] )
+    {
+        return index;
+    }
+    if ( index + 1 == size )
+    {
+        return -1;
+    }
+
+    return Find( value, arr, size, index+1 );
+}
+
+void Display( string arr[], int size )
+{
+    for ( int i = 0; i < size; i++ )
+    {
+        cout << arr[i] << endl;
+    }
+}
+
+// Recursive
+void Display( string arr[], int size, int index )
+{
+    cout << arr[index] << endl;
+
+    if ( index + 1 == size )
+    {
+        return;
+    }
+    Display( arr, size, index+1 );
+}
+
 int main()
 {
     string animals[5] = { "cat", "dog", "ferret", "snake", "bird" };
+
+    Display( animals, 5, 0 );
+
+    cout << endl << endl;
 
     int index1 = Find( "cat", animals, 5 );
     int index2 = Find( "skunk", animals, 5 );
 
     cout << "Cat at " << index1 << endl;
     cout << "Skunk at " << index2 << endl;
+
+    int index3 = Find( "cat", animals, 5, 0 );
+    int index4 = Find( "skunk", animals, 5, 0 );
+
+    cout << "Cat at " << index3 << endl;
+    cout << "Skunk at " << index4 << endl;
 
     cout << "End" << endl;
 
